8a22cf6dfb06c0003e4fe59eb9b19203e4cf9ff9
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-06 12:40:33 +0100

    Added first code example.

---
+++ examples/ipdb-example.py
@@ -0,0 +1,11 @@
+def addition(one, other):
+    import ipdb; ipdb.set_trace()
+
+    print 'Calculating now'
+
+    return one + other
+
+print 'Starting calculation'
+addition(2, 5)
+
+print 'Calculation done'
