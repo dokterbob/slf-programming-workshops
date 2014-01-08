e4ee674773b5b1874fd57870b2b76c6482730e36
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-08 15:04:53 +0100

    Sample setup.py

---
+++ examples/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'snowball',
+    version = '0.1dev',
+    packages = find_packages(),
+    license = 'BSD',
+    long_description = open('README.rst').read(),
+)
