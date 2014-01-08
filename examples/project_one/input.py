7828fa2650feb70b4020c680520729cc52c39915
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-08 17:38:43 +0100

    Input part completed.

--- examples/project_one/input.py
+++ examples/project_one/input.py
@@ -1,6 +1,9 @@
-def read_file(filename):
+def import_data(filename):
     # Open the file
     file_obj = open(filename)
+
+    contents = []
+    imported_lines = 0
 
     # Iterate over lines in the file
     for line in file_obj:
@@ -12,14 +15,21 @@ def read_file(filename):
         if len(numbers) != 2:
             # Convert strings to numbers
 
+            # Assert that the column number corresponds to our
+            # expectations.
+            assert len(numbers) == 103, 'Unkown column number.'
+
             # map() calls the first argument on every item in the second
             # argument and returns a list of results.
             numbers = map(float, numbers)
+
+            contents.append(numbers)
+            imported_lines += 1
         else:
             # We're processing a header
             print 'Skipping header line'
 
-    return contents
+    assert imported_lines == len(contents)
+    print 'Imported %d lines' % imported_lines
 
-# Just for debugging purposes
-read_file('data.txt')
+    return contents
